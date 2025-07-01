d9410b62d93625a756afea2f018f2fd36d4a3db5
Yipeng Wang <n01625427@humber.ca>
2025-06-30 21:54:55 -0400

    try again

---
+++ api/server.js
@@ -0,0 +1,9 @@
+const app = require('../app');
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://${HOST}:${PORT}`)
+});
--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-require('dotenv').config();
-
-const PORT = process.env.PORT;
-const HOST = process.env.HOST;
-
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://${HOST}:${PORT}`)
-});
